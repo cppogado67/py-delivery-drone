233f49badabb932a97ced589c64cebf089485862
claro <c_ppogado67@yahoo.com>
2026-09-23 13:10:47 +0800

    xx00

--- app/main.py
+++ app/main.py
@@ -1,5 +1,56 @@
 class Cargo:
-    def __init__(self, weight: int) -> None:
+    def __init__(self, weight):
         self.weight = weight
 
-# write your code here
+class BaseRobot:
+    def __init__(self, name, weight, coords=None):
+        self.name = name
+        self.weight = weight
+        self.coords = coords if coords else [0, 0]
+    
+    def go_forward(self, step=1):
+        self.coords[1] += step
+    
+    def go_back(self, step=1):
+        self.coords[1] -= step
+    
+    def go_right(self, step=1):
+        self.coords[0] += step
+    
+    def go_left(self, step=1):
+        self.coords[0] -= step
+    
+    def get_info(self):
+        return f"Robot: {self.name}, Weight: {self.weight}"
+
+class FlyingRobot(BaseRobot):
+    def __init__(self, name, weight, coords=None):
+        # Handle 3D coords - if 2 coords passed, add 0 for z
+        if coords and len(coords) == 2:
+            coords = coords + [0]
+        elif not coords:
+            coords = [0, 0, 0]
+        super().__init__(name, weight, coords)
+    
+    def go_up(self, step=1):
+        self.coords[2] += step
+    
+    def go_down(self, step=1):
+        self.coords[2] -= step
+
+class DeliveryDrone(FlyingRobot):
+    def __init__(self, name, weight, coords=None, max_load_weight=0, current_load=None):
+        super().__init__(name, weight, coords)
+        self.max_load_weight = max_load_weight
+        self.current_load = current_load
+        # If Cargo was passed, try to hook it
+        if current_load is not None:
+            self.hook_load(current_load)
+    
+    def hook_load(self, cargo):
+        # Check both conditions before saving
+        if self.current_load is None and cargo.weight <= self.max_load_weight:
+            self.current_load = cargo
+    
+    def unhook_load(self):
+        self.current_load = None
